dd806a70f202484ad6c193b563ef39ce41776e93
PaulPerez306 <PaulPerez306@gmail.com>
2021-11-21 17:30:45 +0100

    punkt na mapie
    lokalizacja dziala

--- Lokalizacja22/app/src/main/java/com/example/lokalizacja2/MapsActivity.java
+++ Lokalizacja22/app/src/main/java/com/example/lokalizacja2/MapsActivity.java
@@ -19,12 +19,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.lokalizacja2.databinding.ActivityMapsBinding;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private ActivityMapsBinding binding;
 
     private LocationManager locationManager;
     private LocationListener locationListener;
@@ -37,8 +36,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+
         setContentView(R.layout.activity_maps);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
