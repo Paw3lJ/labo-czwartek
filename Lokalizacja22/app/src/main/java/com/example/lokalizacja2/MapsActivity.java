2f1997a6eb12c26f36bf0f1b727bd327514ced70
PaulPerez306 <PaulPerez306@gmail.com>
2021-11-27 16:08:32 +0100

    testowanie przesyłania bluetooth

--- Lokalizacja22/app/src/main/java/com/example/lokalizacja2/MapsActivity.java
+++ Lokalizacja22/app/src/main/java/com/example/lokalizacja2/MapsActivity.java
@@ -2,6 +2,13 @@ package com.example.lokalizacja2;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 import android.Manifest;
@@ -9,7 +16,12 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Handler;
+import android.os.Message;
 import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
@@ -20,9 +32,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    //private static final int REQUEST_ENABLE_BT=1;
     private GoogleMap mMap;
 
     private LocationManager locationManager;
@@ -31,6 +49,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private final long MIN_TIME = 1000;
     private final long MIN_DIST = 5;
     private LatLng latLng;
+    private BroadcastReceiver receiver;
+    private int REQUEST_ENABLE_BT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +67,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS}, PackageManager.PERMISSION_GRANTED);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
+
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        //BroadcastReceiver receiver;
+        registerReceiver(receiver, filter);
+
+        // Create a BroadcastReceiver for ACTION_FOUND.
+        final BroadcastReceiver receiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    String deviceName = device.getName();
+                    String deviceHardwareAddress = device.getAddress(); // MAC address
+                }
+            }
+        };
     }
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+
+            unregisterReceiver(receiver);
+    }
+
+
 
     /**
      * Manipulates the map once available.
@@ -107,6 +155,132 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         catch (SecurityException e){
             e.printStackTrace();
+        }
+
+    }
+    public void getLocationDetails(View view)
+    {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+
+        int requestCode = 1;
+        Intent discoverableIntent =
+                new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivityForResult(discoverableIntent, requestCode);
+
+         class MyBluetoothService {
+            private static final String TAG = "MY_APP_DEBUG_TAG";
+            private Handler handler; // handler that gets info from Bluetooth service
+
+            // Defines several constants used when transmitting messages between the
+            // service and the UI.
+            class MessageConstants {
+                public static final int MESSAGE_READ = 0;
+                public static final int MESSAGE_WRITE = 1;
+                public static final int MESSAGE_TOAST = 2;
+
+                // ... (Add other message types here as needed.)
+            }
+
+             class ConnectedThread extends Thread {
+                private final BluetoothSocket mmSocket;
+                private final InputStream mmInStream;
+                private final OutputStream mmOutStream;
+                private byte[] mmBuffer; // mmBuffer store for the stream
+
+                public ConnectedThread(BluetoothSocket socket) {
+                    mmSocket = socket;
+                    InputStream tmpIn = null;
+                    OutputStream tmpOut = null;
+
+                    // Get the input and output streams; using temp objects because
+                    // member streams are final.
+                    try {
+                        tmpIn = socket.getInputStream();
+                    } catch (IOException e) {
+                        Log.e(TAG, "Error occurred when creating input stream", e);
+                    }
+                    try {
+                        tmpOut = socket.getOutputStream();
+                    } catch (IOException e) {
+                        Log.e(TAG, "Error occurred when creating output stream", e);
+                    }
+
+                    mmInStream = tmpIn;
+                    mmOutStream = tmpOut;
+                }
+
+                public void run() {
+                    mmBuffer = new byte[1024];
+                    int numBytes; // bytes returned from read()
+
+                    // Keep listening to the InputStream until an exception occurs.
+                    while (true) {
+                        try {
+                            // Read from the InputStream.
+                            numBytes = mmInStream.read(mmBuffer);
+                            // Send the obtained bytes to the UI activity.
+                            Message readMsg = handler.obtainMessage(
+                                    MessageConstants.MESSAGE_READ, numBytes, -1,
+                                    mmBuffer);
+                            readMsg.sendToTarget();
+                        } catch (IOException e) {
+                            Log.d(TAG, "Input stream was disconnected", e);
+                            break;
+                        }
+                    }
+                }
+
+                // Call this from the main activity to send data to the remote device.
+                public void write(byte[] bytes) {
+                    try {
+                        mmOutStream.write(bytes);
+
+                        // Share the sent message with the UI activity.
+                        Message writtenMsg = handler.obtainMessage(
+                                MessageConstants.MESSAGE_WRITE, -1, -1, mmBuffer);
+                        writtenMsg.sendToTarget();
+                    } catch (IOException e) {
+                        Log.e(TAG, "Error occurred when sending data", e);
+
+                        // Send a failure message back to the activity.
+                        Message writeErrorMsg =
+                                handler.obtainMessage(MessageConstants.MESSAGE_TOAST);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("toast",
+                                "Couldn't send data to the other device");
+                        writeErrorMsg.setData(bundle);
+                        handler.sendMessage(writeErrorMsg);
+                    }
+                }
+
+                // Call this method from the main activity to shut down the connection.
+                public void cancel() {
+                    try {
+                        mmSocket.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, "Could not close the connect socket", e);
+                    }
+                }
+            }
         }
 
     }
